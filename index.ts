b78c4cae5919d29b2c6783f1d94d87db725bf891
rosancoderian <rosancoderian@gmail.com>
2024-02-02 11:37:10 +0700

    chore: move controllers

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import fastify from 'fastify'
-import { createUser, deleteUser, updateUser } from './modules/users'
-import ct from 'countries-and-timezones'
-import date from './utils/date'
+import { deleteUserByUserId, postUser, putUserByUserId } from './modules/users'
 
 const app = fastify()
 app.listen({ port: 3000 }, (err, addr) => {
@@ -13,69 +11,8 @@ app.listen({ port: 3000 }, (err, addr) => {
   console.log(`Listening on ${addr}`)
 })
 
-app.get('/', (req, rep) => {
-  rep.status(200).send('ok')
-})
+app.get('/', (_, rep) => rep.status(200).send('ok'))
 
-type UserBody = {
-  pob: string
-  d: string
-  m: string
-  y: string
-  firstname: string
-  lastname: string
-  email: string
-}
-
-app.post<{ Body: UserBody }>('/user', async (req, rep) => {
-  try {
-    const { pob, d, m, y, firstname, lastname, email } = req.body
-    const [tz] = ct.getTimezonesForCountry(pob) || []
-    const dob = date.tz(`${y}-${m}-${d}`, tz.name).utc().format()
-    const data = {
-      pob,
-      firstname,
-      lastname,
-      dob,
-      email,
-    }
-    const createdUser = await createUser(data)
-    rep.status(201).send(createdUser)
-  } catch (error) {
-    console.error(error)
-    rep.status(500).send({ error })
-  }
-})
-
-app.delete<{ Params: { userId: string } }>(
-  '/user/:userId',
-  async (req, rep) => {
-    try {
-      await deleteUser(Number(req.params.userId))
-      rep.status(204).send()
-    } catch (error) {
-      console.error(error)
-      rep.status(500).send({ error })
-    }
-  }
-)
-
-app.put<{ Params: { userId: string }; Body: UserBody }>(
-  '/user/:userId',
-  async (req, rep) => {
-    try {
-      const { pob, d, m, y, ...body } = req.body
-      let data = {}
-      if (pob && d && m && y) {
-        const [tz] = ct.getTimezonesForCountry(pob) || []
-        const dob = date.tz(`${y}-${m}-${d}`, tz.name).utc().format()
-        data = { pob, dob }
-      }
-      await updateUser(Number(req.params.userId), {...data, ...body})
-      rep.status(200).send()
-    } catch (error) {
-      console.error(error)
-      rep.status(500).send({ error })
-    }
-  }
-)
+app.post('/user', postUser)
+app.delete('/user/:userId', deleteUserByUserId)
+app.put('/user/:userId', putUserByUserId)
--- modules/users/index.ts
+++ modules/users/index.ts
@@ -1,6 +1,19 @@
 import { eq } from 'drizzle-orm'
 import { db } from '../../db/connection'
+import ct from 'countries-and-timezones'
+import date from '../../utils/date'
 import * as Schema from '../../db/schema'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+type UserBody = {
+  pob: string
+  d: string
+  m: string
+  y: string
+  firstname: string
+  lastname: string
+  email: string
+}
 
 export const createUser = async (user: Schema.NewUser) => {
   const [createdUser]: Schema.User[] = await db
@@ -17,10 +30,69 @@ export const deleteUser = async (userId: number) => {
     .returning()
 }
 
-export const updateUser = async (userId: number, user: Partial<Schema.NewUser>) => {
+export const updateUser = async (
+  userId: number,
+  user: Partial<Schema.NewUser>
+) => {
   return await db
     .update(Schema.users)
     .set(user)
     .where(eq(Schema.users.id, userId))
     .returning()
+}
+
+export const postUser = async (
+  req: FastifyRequest<{ Body: UserBody }>,
+  rep: FastifyReply
+) => {
+  try {
+    const { pob, d, m, y, firstname, lastname, email } = req.body
+    const [tz] = ct.getTimezonesForCountry(pob) || []
+    const dob = date.tz(`${y}-${m}-${d}`, tz.name).utc().format()
+    const data = {
+      pob,
+      firstname,
+      lastname,
+      dob,
+      email,
+    }
+    const createdUser = await createUser(data)
+    rep.status(201).send(createdUser)
+  } catch (error) {
+    console.error(error)
+    rep.status(500).send({ error })
+  }
+}
+
+export const deleteUserByUserId = async (
+  req: FastifyRequest<{ Params: { userId: string } }>,
+  rep: FastifyReply
+) => {
+  try {
+    await deleteUser(Number(req.params.userId))
+    rep.status(204).send()
+  } catch (error) {
+    console.error(error)
+    rep.status(500).send({ error })
+  }
+}
+
+export const putUserByUserId = async (
+  req: FastifyRequest<{ Params: { userId: string }; Body: UserBody }>,
+  rep: FastifyReply
+) => {
+  try {
+    const { pob, d, m, y, ...body } = req.body
+    let data = {}
+    if (pob && d && m && y) {
+      const [tz] = ct.getTimezonesForCountry(pob) || []
+      const dob = date.tz(`${y}-${m}-${d}`, tz.name).utc().format()
+      data = { pob, dob }
+    }
+    await updateUser(Number(req.params.userId), { ...data, ...body })
+    rep.status(200).send()
+  } catch (error) {
+    console.error(error)
+    rep.status(500).send({ error })
+  }
 }
